1ba0d3a9b2098148fcf2a7098cb2d8d9de0fdb77
khadir108 <mohammadkhadir@outlook.com>
2020-03-25 14:15:24 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+# This is the provider used to spin up the gcloud instance
+provider "google" {
+  #credentials = file("terraform.json")
+  project = "hip-plexus-238517"
+  region  = "us-central1"
+  
+}
+
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "tmp-vm"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  metadata_startup_script = file("startup.sh")
+   
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
